5bb6f9f98fb00c1a61aeacbf34cc6e64e958a0f1
Tyler Tsang <tylertsang101@gmail.com>
2020-01-01 20:09:00 -0500

    Uploading Practice Files to log Practice sessions
    Adding File structure for practice, basic explorations, and guided walkthroughs

---
+++ Practice/Airbnb_Exploration.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+from plotly import __version__
+
---
+++ Practice/Cufflinks + Plotly_Exploration.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+from plotly import __version__
+import cufflinks as cf
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+init_notebook_mode(connected=True)
+cf.go_offline()
+
